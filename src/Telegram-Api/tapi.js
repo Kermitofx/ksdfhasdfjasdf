87e965a2356e3eba1bfd09972e39f1fb0dc0a760
dandandan10 <57716377+dandandan10@users.noreply.github.com>
2019-11-23 11:04:18 -0300

    Update tapi.js

--- src/Telegram-Api/tapi.js
+++ src/Telegram-Api/tapi.js
@@ -1,3 +1,3 @@
-const Token = 'BoToken';
+const Token = '1033720100:AAEJqctB3xt4eSqT5QHbm_mSJN---SDL7mM';
 const TelegramBot = require('node-telegram-bot-api');
 exports.bot = new TelegramBot(Token, {polling: true});
