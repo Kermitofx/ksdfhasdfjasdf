d65caa01e98df50b1a7311d6e01511365765a10c
dandandan10 <57716377+dandandan10@users.noreply.github.com>
2019-12-14 20:07:04 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("./Telegram-Api/node_modules/mongoose");
-mongoose.connect("mongodb+srv://botbotgg:ehXV5uU3G2mZgpLy@cluster0-wxnvj.gcp.mongodb.net/test?retryWrites=true&w=majority
-");
+mongoose.connect("mongodb+srv://botbot:izMsQUBCwf3BuDTB@cluster0-cmcyi.mongodb.net/test?retryWrites=true&w=majority");
 
 const rmodel = require("./Model/replys");
 
