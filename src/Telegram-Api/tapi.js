78c6c24e72efb4c3e7a769447e00f0d9dbc88386
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2020-07-19 18:56:49 -0300

    Update tapi.js

--- src/Telegram-Api/tapi.js
+++ src/Telegram-Api/tapi.js
@@ -1,3 +1,3 @@
-const Token = '1033720100:AAEJqctB3xt4eSqT5QHbm_mSJN---SDL7mM';
+const Token = '1136687058:AAGEPw6RuvOW4JCXGWP8X5u-ABnncptTzHc';
 const TelegramBot = require('node-telegram-bot-api');
 exports.bot = new TelegramBot(Token, {polling: true});
